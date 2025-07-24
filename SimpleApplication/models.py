1e6ffe5014b8c9db8be2fd3b546db095ab9b87fe
ParthaSarathi <parthuprince112@gmail.com>
2025-07-24 15:19:27 +0530

    migrate

--- SimpleApplication/models.py
+++ SimpleApplication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# Example model for a simple application
+
+
+class Home(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    img = models.ImageField(upload_to='images/')
